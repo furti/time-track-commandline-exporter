1b56c450ba4665ec23db1e68d195b5fcbbc9c004
Daniel Furtlehner <daniel.furtlehner@porscheinformatik.at>
2019-01-28 10:26:20 +0100

    Print time slots (comming, going, pause) for each day

--- index.js
+++ index.js
@@ -19,35 +19,79 @@
 var chalk = require("chalk"),
   timeTrack = require("time-track");
 
-function getMinMaxTime(tasks) {
-  var min = null;
-  var max = null;
+const TIME_SLOT_MIN_PAUSE_MILLIS = 1000 * 60 * 10;
+
+function getTimeSlots(tasks) {
+  var allTimes = []
 
   tasks.forEach(function(task) {
     if (task.times) {
       task.times.forEach(function(time) {
-        if (!min || min > time.start) {
-          min = time.start;
-        }
-
-        if (!max || max < time.end) {
-          max = time.end;
-        }
+        allTimes.push(time);
       });
     }
   });
 
-  return {
-    min: min,
-    max: max
-  };
+  allTimes.sort((a, b) => {
+    aDate = a.start;
+    bDate = b.start;
+
+    return aDate.getTime() - bDate.getTime();
+  });
+
+  const timeSlots = []
+  let actualSlot = null;
+
+  allTimes.forEach(entry => {
+    if(actualSlot === null) {
+      actualSlot = {
+        start: entry.start,
+        end: entry.end
+      }
+    }
+    else {
+      const lastEndTime = actualSlot.end.getTime();
+      const nextStartTime = entry.start.getTime();
+      const pauseInMillis = nextStartTime - lastEndTime;
+
+      if(pauseInMillis >= TIME_SLOT_MIN_PAUSE_MILLIS) {
+        // Start a new slot when the pause is too big
+        actualSlot.pauseInMillis = pauseInMillis;
+        timeSlots.push(actualSlot);
+
+        actualSlot = {
+          start: entry.start,
+          end: entry.end
+        }
+      }
+      else {
+        // Otherwise update the end time of the actual slot
+        actualSlot.end = entry.end
+      }
+    }
+  });
+
+  if(actualSlot !== null) {
+    timeSlots.push(actualSlot);
+  }
+
+  return timeSlots;
 }
 
 module.exports = function(tasks) {
-  var minMax = getMinMaxTime(tasks);
+  var timeSlots = getTimeSlots(tasks);
 
-  console.log(chalk.green("Start: " + minMax.min));
-  console.log(chalk.green("End: " + minMax.max));
+  for(let timeSlot of timeSlots)
+  {
+    console.log(chalk.green("Start: " + timeSlot.start));
+    console.log(chalk.green("End: " + timeSlot.end));
+
+    if(timeSlot.pauseInMillis) {
+      console.log(chalk.yellow("Pause: " + Math.round(timeSlot.pauseInMillis / 1000 / 60) + " min"));
+    }
+    
+    console.log('');
+  }
 
   tasks.forEach(function(task) {
     console.log(
